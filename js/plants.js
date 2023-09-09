b2f0c81c813e0c41862c84e1140b1b26549bbf86
darian-b1031 <143450071+darian-b1031@users.noreply.github.com>
2023-09-09 17:11:57 -0400

    Add files via upload

---
+++ js/plants.js
@@ -0,0 +1,9 @@
+/*
+WDD 221 WK2 Lab
+Author: Darian Bolin
+Date: September 9, 2023
+*/
+
+let blanket = "images/blanket.jpg";
+let bluestem = "images/bluestem.jpg";
+let rugosa = "images/rugosa.jpg";
